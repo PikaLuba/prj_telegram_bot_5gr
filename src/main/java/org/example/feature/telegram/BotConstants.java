b2c79df896c0abe6d2e080db1a23aa9f934aed12
PikaLubov <150253436+PikaLubov@users.noreply.github.com>
2024-03-14 22:49:24 +0200

    add progect

---
+++ src/main/java/org/example/feature/telegram/BotConstants.java
@@ -0,0 +1,9 @@
+package org.example.feature.telegram;
+
+public class BotConstants {
+
+    // ВАШ ТЕЛЕГРАМ БОТ
+    public static final String BOT_NAME = "NNN";
+    public static final String BOT_TOKEN = "NNN";
+}
+
