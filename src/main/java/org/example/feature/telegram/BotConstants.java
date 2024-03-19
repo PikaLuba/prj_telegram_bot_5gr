cd15ccce0d5a4bd81de7ba654b22d695a8aa662a
PikaLuba <157685096+PikaLuba@users.noreply.github.com>
2024-03-19 07:19:12 +0200

    Update BotConstants.javaффф

--- src/main/java/org/example/feature/telegram/BotConstants.java
+++ src/main/java/org/example/feature/telegram/BotConstants.java
@@ -3,7 +3,7 @@ package org.example.feature.telegram;
 public class BotConstants {
 
     // ВАШ ТЕЛЕГРАМ БОТ
-    public static final String BOT_NAME = "GoITJavaLPCurrencyBot";
-    public static final String BOT_TOKEN = "6165759381:AAFjcxzg9JkRrmDAsN-ixe1TxqkmVjHmyeo";
+    public static final String BOT_NAME = "";
+    public static final String BOT_TOKEN = "";
 }
 
